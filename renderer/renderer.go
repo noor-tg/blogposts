13effc2c707a1e6cd0979b93ece8837b5b71064c
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-21 19:03:49 +0200

    docs: comment on code

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -8,12 +8,16 @@ import (
 )
 
 func Render(w io.Writer, post reader.Post) error {
+	// parse template from embedded fs
 	templ, err := template.ParseFS(blogposts.PostTemplate, "templates/*.gohtml")
 
 	if err != nil {
 		return err
 	}
 
+	// exec template with post data
+	// handle error if exist
+	// store returned string from rendered template to buffer
 	if err := templ.Execute(w, post); err != nil {
 		return err
 	}
